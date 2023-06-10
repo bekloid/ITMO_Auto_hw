08f907f54283a10fea373867ce627f8619c68554
bekloid <bekloid@gmail.com>
2023-06-10 23:21:01 +0300

    Первая задача решена.Ура

--- home_work/4_hw.py
+++ home_work/4_hw.py
@@ -1,24 +1,46 @@
 # задача 1
+#
+# class Rectangle():
+#     def __init__(self,width,height):
+#         self.width = width
+#         self.height = height
+#
+#     def sq(self):
+#         return int(self.width) * int(self.height)
+#     def per(self):
+#         return (self.width + self.height) * 2
+#
+# # я все еще не понимаю, как это работает
+# p_1 = Rectangle(5, 7)
+# p_2 = Rectangle(9, 3)
+# p_3 = Rectangle(35,4)
+# print(p_1.sq())
+# print(p_1.per())
+# print(p_2.sq())
+# print(p_2.per())
+# print(p_3.sq())
+# print(p_3.per())
 
-class Rectangle():
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
+# Задача 2
 
-    def sq(self):
-        return int(self.width) * int(self.height)
-    def per(self):
-        return (self.width + self.height) * 2
+class Math():
 
-# я все еще не понимаю, как это работает
-p_1 = Rectangle(5, 7)
-p_2 = Rectangle(9, 3)
-p_3 = Rectangle(35,4)
-print(p_1.sq())
-print(p_1.per())
-print(p_2.sq())
-print(p_2.per())
-print(p_3.sq())
-print(p_3.per())
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        print(int(self.a) + int(self.b))
+
+
+    def multiplication(self):
+        print(int(self.a) * int(self.b))
+
+
+    def division(self):
+        print(int(self.a) / int(self.b))
+
+    def subtraction(self):
+        print(int(self.a) - int(self.b))
 
 
