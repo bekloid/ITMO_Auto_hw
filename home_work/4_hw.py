e06910790a3cd5b6aa72a8e3ed0533dd1d9dea95
bekloid <bekloid@gmail.com>
2023-06-11 23:44:37 +0300

    Домашняя работа. Задание 4

--- home_work/4_hw.py
+++ home_work/4_hw.py
@@ -1,25 +1,25 @@
-# задача 1
-#
-# class Rectangle():
-#     def __init__(self,width,height):
-#         self.width = width
-#         self.height = height
-#
-#     def sq(self):
-#         return int(self.width) * int(self.height)
-#     def per(self):
-#         return (self.width + self.height) * 2
-#
-# # я все еще не понимаю, как это работает
-# p_1 = Rectangle(5, 7)
-# p_2 = Rectangle(9, 3)
-# p_3 = Rectangle(35,4)
-# print(p_1.sq())
-# print(p_1.per())
-# print(p_2.sq())
-# print(p_2.per())
-# print(p_3.sq())
-# print(p_3.per())
+#задача 1
+
+class Rectangle():
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    def sq(self):
+        return int(self.width) * int(self.height)
+    def per(self):
+        return (self.width + self.height) * 2
+
+# я все еще не понимаю, как это работает
+p_1 = Rectangle(5, 7)
+p_2 = Rectangle(9, 3)
+p_3 = Rectangle(35,4)
+print(p_1.sq())
+print(p_1.per())
+print(p_2.sq())
+print(p_2.per())
+print(p_3.sq())
+print(p_3.per())
 
 # Задача 2
 
@@ -43,4 +43,6 @@ class Math():
     def subtraction(self):
         print(int(self.a) - int(self.b))
 
+
+#задача 4
 
